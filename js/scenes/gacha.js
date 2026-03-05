d92399fe765ec16e7ca6e18b07ff586c7b8fef1f
鈴木崇矢 <s.takamaru.1020@gmail.com>
2026-03-06 00:15:56 +0900

    ガチャ画面制作

--- js/scenes/gacha.js
+++ js/scenes/gacha.js
@@ -1,6 +1,10 @@
 // js/scenes/gacha.js
 
+import { gachaData } from "../data/gachaData.js";
+
+
 export function initGacha() {
+    let currentGacha = "pickup";
     const listItems = document.querySelectorAll(".gacha-list-item");
     const previewText = document.querySelector(".gacha-placeholder-text");
     const previewImg = document.getElementById("gacha-preview-img");
@@ -12,10 +16,10 @@ export function initGacha() {
             // クリックされた要素に active をつける
             e.currentTarget.classList.add("active");
 
-            const gachaName = e.currentTarget.textContent;
+            currentGacha = e.currentTarget.dataset.gacha;
 
             // 右側のプレビュー切り替えロジック
-            if (gachaName === "ノーマルガチャ") {
+            if (currentGacha === "normal") {
                 // ノーマルガチャの場合、画像を表示してテキストを隠す
                 if (previewImg) {
                     previewImg.src = "assets/images/normal_gacha.png";
@@ -27,9 +31,76 @@ export function initGacha() {
                 if (previewImg) previewImg.style.display = "none";
                 if (previewText) {
                     previewText.style.display = "block";
-                    previewText.textContent = `${gachaName}（未作成）`;
+                    previewText.textContent = `${currentGacha}（未作成）`;
                 }
             }
         });
+    });
+
+    const rateBtn = document.querySelector(".gacha-info-btn");
+    const rateModal = document.getElementById("gacha-rate-modal");
+    const rateCloseBtn = document.getElementById("rate-close-btn");
+
+    if (rateBtn && rateModal) {
+        rateBtn.addEventListener("pointerdown", () => {
+
+            const table = document.querySelector(".rate-table");
+            if (!table) return;
+            table.innerHTML = "";
+
+            const rates = gachaData[currentGacha].rarityRates;
+
+            rates.forEach(r => {
+
+                const row = document.createElement("tr");
+
+                row.innerHTML = `
+                    <td>${r.rarity}</td>
+                    <td>${r.rate}</td>
+                `;
+
+                table.appendChild(row);
+            });
+
+            rateModal.style.display = "flex";
+        });
+    }
+
+    if (rateCloseBtn && rateModal) {
+        rateCloseBtn.addEventListener("pointerdown", () => {
+            rateModal.style.display = "none";
+        });
+    }
+
+    const drawButtons = document.querySelectorAll(".gacha-draw-btn");
+    const confirmModal = document.getElementById("gacha-confirm-modal");
+    const cancelBtn = document.getElementById("cancel-gacha");
+
+    drawButtons.forEach(btn => {
+
+        btn.addEventListener("pointerdown", () => {
+
+            const count = Number(btn.dataset.count);
+
+            let cost;
+            if (count === 1) {
+                cost = 100;
+            } else if (count === 10) {
+                cost = 1000;
+            }
+
+            const diamonds = 500;
+
+            document.getElementById("cost-before").textContent = cost;
+            document.getElementById("diamond-before").textContent = diamonds;
+            document.getElementById("diamond-after").textContent = diamonds - cost;
+
+            confirmModal.style.display = "flex";
+        });
+
+    });
+
+    cancelBtn.addEventListener("pointerdown", () => {
+        confirmModal.style.display = "none";
     });
 }
